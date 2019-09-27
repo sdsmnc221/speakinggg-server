7102feeb717e1f7d809bcf574f4e5d85595b6caf
An Truong <antr.2201@gmail.com>
2019-09-27 11:33:29 +0200

    update

--- src/messages.js
+++ src/messages.js
@@ -1,70 +1,69 @@
-const { ERRORS } = require('./const.errors')
+const { ERRORS } = require('./const.errors');
 
-let MESSAGES = []
+let MESSAGES = [];
 
-function isValid (message) {
-  return typeof message === 'string'
-    && message.length > 0
-    && message.length < 500
+function isValid(message) {
+  return (
+    typeof message === 'string' && message.length > 0 && message.length < 500
+  );
 }
 
-function isCommand (message) {
-  return typeof message === 'string' && message[0] === '/'
+function isCommand(message) {
+  return typeof message === 'string' && message[0] === '/';
 }
 
-function isSpam (socket) {
+function isSpam(socket) {
   if (socket.lastMessage && socket.lastMessage > Date.now() - 2000) {
-    if (socket.tries > 3) return true
-    else socket.tries += 1
+    if (socket.tries > 3) return true;
+    else socket.tries += 1;
   } else {
-    socket.tries = 0
+    socket.tries = 0;
   }
-  socket.lastMessage = Date.now()
-  return false
+  socket.lastMessage = Date.now();
+  return false;
 }
 
-function getCommand (message) {
+function getCommand(message) {
   return {
     command: 'commandName',
     value: 'someValue'
-  }
+  };
 }
 
-function formatMessage (socket, message, isCommand = false) {
+function formatMessage(socket, message, isCommand = false) {
   return {
     type: isCommand ? 'command' : 'message',
     text: message,
-    created: new Date(),
-    user: socket.user
-  }
+    created: new Date()
+  };
 }
 
 exports.default = {
-  add (message, socket) {
-    console.log('will check if spam')
+  add(message, socket) {
+    console.log('will check if spam');
     if (isSpam(socket)) {
-      return Promise.reject(ERRORS.MESSAGE_SPAMMING)
+      return Promise.reject(ERRORS.MESSAGE_SPAMMING);
     }
     // TODO: Strip message spaces
     if (!isValid(message)) {
-      return Promise.reject(ERRORS.MESSAGE_INVALID)
+      return Promise.reject(ERRORS.MESSAGE_INVALID);
     }
     if (isCommand(message)) {
       return Promise.resolve({
         message: formatMessage(socket, message, true)
-      })
+      });
     }
-    const m = formatMessage(socket, message)
-    MESSAGES.push(m)
+    const m = formatMessage(socket, message);
+    MESSAGES.push(m);
     if (MESSAGES.length > 50) {
-      MESSAGES = MESSAGES.slice(-50)
+      MESSAGES = MESSAGES.slice(-50);
     }
-    return Promise.resolve({ 
-      message: m, 
-      messages: MESSAGES 
-    })
+    return Promise.resolve({
+      message: m,
+      messages: MESSAGES
+    });
   },
-  get () {
-    return MESSAGES
+  get() {
+    return MESSAGES;
   }
-}
+};
--- src/socket.js
+++ src/socket.js
@@ -1,149 +1,63 @@
-const usersList = require('./users').LIST
-const users = require('./users').default
-const messages = require('./messages').default
-const { EVENTS_IN, EVENTS_OUT } = require('./const.events')
-const { ERRORS, ERROR_MESSAGES } = require('./const.errors')
+const messages = require('./messages').default;
+const { EVENTS_IN, EVENTS_OUT } = require('./const.events');
+const { ERRORS, ERROR_MESSAGES } = require('./const.errors');
 
-exports.default = (io) => (socket) => new Socket(socket, io)
+exports.default = io => socket => new Socket(socket, io);
 
 class Socket {
-  constructor (socket, io) {
-    this.io = io
-    this.socket = socket
+  constructor(socket, io) {
+    this.io = io;
+    this.socket = socket;
 
-    socket.on(EVENTS_IN.USER_REGISTER, this.onUserRegister.bind(this))
-    socket.on(EVENTS_IN.USER_TYPING, this.onUserTyping.bind(this))
-    socket.on(EVENTS_IN.MESSAGE_NEW, this.onMessageNew.bind(this))
-    socket.on('disconnect', this.onDisconnect.bind(this))
+    socket.on(EVENTS_IN.MESSAGE_NEW, this.onMessageNew.bind(this));
   }
 
-  // Events 
+  // Events
 
-  onUserRegister (data) {
-    console.log('user registering')
-    if (this.socket.user) {
-      this.emitError(ERRORS.ALREADY_CONNECTED)
-      return
-    }
-    users.register(this.socket, data)
-      .then((user) => {
-        this.socket.user = user
-        this.emitUserAdd(user)
-        this.emitMessagesUpdate()
-        console.log('user registered', user)
+  onMessageNew(data) {
+    messages
+      .add(data, this.socket)
+      .then(data => {
+        this.emitMessage(data);
       })
-      .catch((errCode) => {
-        console.error('Error onUserRegister', errCode)
-        this.emitError(errCode)
-      })
-  }
-
-  onUserTyping () {
-    this.emitTyping(true)
-    users.typing(this.socket)
-      .then((wasTyping) => {
-        this.emitTyping(false)
-      })
-      .catch((err) => {
-        // Whatever
-      })
-  }
-
-  onMessageNew (data) {
-    if (!this.socket.user) {
-      console.error('Some dude try to send message before login')
-      return
-    }
-    const username = this.socket.user.username
-    const user = users.getUserFromUsername(username)
-
-    messages.add(data, this.socket)
-      .then((data) => {
-        this.emitMessage(data)
-
-        if (this.socket.typing) {
-          this.socket.typing = false
-          user.typing = false
-          this.emitTyping(false)
-        }
-      })
-      .catch((errorCode) => {
-        this.emitError(errorCode)
-        console.error('Error sendMessage')
+      .catch(errorCode => {
+        this.emitError(errorCode);
+        console.error('Error sendMessage');
         // Message can't be sent for some reason
-      })
-  }
-
-  onDisconnect() {
-    const user = this.socket.user
-    if (user) {
-      users.unregister(this.socket)
-        .then((deletedUser) => {
-          console.log(this.socket.user.username + ' has disconnected.')
-          this.emitUserRemove(deletedUser)
-        })
-        .catch((err) => {
-          console.error('Error onDisconnect', err)
-        })
-    }
+      });
   }
 
   // Emits
 
-  emitTyping (typing) {
-    const user = this.socket.user
-    this.io.emit(EVENTS_OUT.USER_TYPING, {
-      user,
-      typing
-    })
-  }
-
-  emitError (errCode) {
+  emitError(errCode) {
     this.socket.emit(EVENTS_OUT.ERROR, {
       code: errCode,
       message: ERROR_MESSAGES[errCode]
-    })
+    });
   }
 
-  emitMessage (data) {
+  emitMessage(data) {
     switch (data.message.type) {
       case 'command': {
-        const regex = /^\/(\w+) ?(.*)$/
-        const exec = regex.exec(data.message.text)
+        const regex = /^\/(\w+) ?(.*)$/;
+        const exec = regex.exec(data.message.text);
         this.io.emit(EVENTS_OUT.COMMAND_NEW, {
           command: exec[1],
           value: exec[2]
-        })
-        break
+        });
+        break;
       }
       case 'message':
       default:
-        this.io.emit(EVENTS_OUT.MESSAGE_NEW, data)
+        this.io.emit(EVENTS_OUT.MESSAGE_NEW, data);
     }
   }
 
   emitMessagesUpdate() {
     this.socket.emit(EVENTS_OUT.MESSAGES_UPDATE, {
       messages: messages.get()
-    })
-  }
-
-  emitUserAdd (user) {
-    this.socket.emit(EVENTS_OUT.USER_REGISTERED, user)
-    this.io.emit(EVENTS_OUT.USERS_UPDATE, {
-      users: usersList,
-      type: 'join',
-      user
-    })
-  }
-
-  emitUserRemove (user) {
-    this.io.emit(EVENTS_OUT.USERS_UPDATE, {
-      users: usersList,
-      type: 'left',
-      user
-    })
+    });
   }
 }
 
-exports.Socket = Socket
+exports.Socket = Socket;
